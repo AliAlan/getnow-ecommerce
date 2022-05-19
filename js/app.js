fbde8d938c810648552dd51b74a5d2b593e6b5f8
vehbi can alan <alanalihaydar@gmail.com>
2022-05-19 13:35:41 +0300

    last commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const mobileSidebar = document.querySelector(".mobile-sidebar");
-const productImage = document.querySelector(".product-image");
-const productName = document.querySelector(".product-name");
-const productPrice = document.querySelector(".product-price");
 
 hamburger.addEventListener("click", () => {
   mobileSidebar.style.clipPath = "circle(141.4% at 100% 100%)";
@@ -11,11 +8,3 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   mobileSidebar.style.clipPath = "circle(0% at 100% 100%)";
 });
-
-fetch("https://dummyjson.com/products")
-  .then((res) => res.json())
-  .then((data) => {
-    data.products.forEach((element) => {
-      console.log(element.title);
-    });
-  });
